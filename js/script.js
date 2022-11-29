a395fadc4ed69a76aea18a6e9a4dff2926cd61fd
Frah01 <vinario.francesco@gmail.com>
2022-11-29 15:31:47 +0100

    Esercizio completo con bonus

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+const container= document.querySelector('.content-boxes')
+
 for(let i = 1; i<=100 ; i++){
     let element= '';
     let div3=(i%3 === 0);
@@ -6,19 +9,23 @@ for(let i = 1; i<=100 ; i++){
 
     if(div3 && div5){
         element = 'fizzbuzz';
+        element= `<div class="box box-red"> ${'fizzbuzz'}</div>`;
     }
 
     else if(div3){
         element = 'fizz';
+        element= `<div class="box box-green"> ${'fizz'}</div>`;
     }
 
     else if(div5){
         element = 'buzz';
+        element= `<div class="box box-orange"> ${'buzz'}</div>`;
     }
 
     else {
         element= i
+        element= `<div class="box box-blue"> ${i}</div>`;
     }
-
+    container.innerHTML+= element;
     console.log(element);
 }
